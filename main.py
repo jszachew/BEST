0508909db1fcbb3e00b5872ed894cfb8831cba08
Jakub Szachewicz <jszachewicz@vewd.com>
2021-05-18 12:52:16 +0200

    Messages UI done

--- main.py
+++ main.py
@@ -2,17 +2,36 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template
+from typing import Callable
 
-app = Flask(__name__)
+
+class MySQLAlchemy(SQLAlchemy):  # Or you can add the below code on the SQLAlchemy directly if you think to modify the package code is acceptable.
+    Column: Callable  # Use the typing to tell the IDE what the type is.
+    String: Callable
+    Integer: Callable
 
 
-@app.route('/message')
-def best():
+app = Flask(__name__, template_folder='template')
+
+messages = []
+
+
+@app.route('/messages')
+def message():
     cookie_v = int(request.cookies.get('PHPSESSID'), 16)
+    message_p = request.args.get("message")
     csrf = int(request.args.get("csrf"), 16)
     result = cookie_v ^ csrf
+
+    messages.append(message_p)
     print(result)
     return str(result)
+
+
+@app.route("/")
+def chat():
+    return render_template('chat.html', messages=messages)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

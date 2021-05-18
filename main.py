28c8f960419427f0587205536259f7b99642bfbf
Jakub Szachewicz <jszachewicz@vewd.com>
2021-05-18 19:26:12 +0200

    file writing added and opening messages

--- main.py
+++ main.py
@@ -2,15 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, render_template
-from typing import Callable
-
-
-class MySQLAlchemy(SQLAlchemy):  # Or you can add the below code on the SQLAlchemy directly if you think to modify the package code is acceptable.
-    Column: Callable  # Use the typing to tell the IDE what the type is.
-    String: Callable
-    Integer: Callable
 
 
 app = Flask(__name__, template_folder='template')
@@ -26,8 +18,10 @@ def message():
     result = cookie_v ^ csrf
 
     messages.append(message_p)
-    print(result)
-    return str(result)
+    f = open("results.txt", "a")
+    f.write(str(result))
+    f.close()
+    return render_template('chat.html', messages=messages)
 
 
 @app.route("/")
